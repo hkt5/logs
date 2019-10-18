2cde5ec05e3c79f65ba55080e9b9f86f8817d232
adrian <r3v@protonmail.ch>
2019-10-18 09:32:48 +0200

    Procfile for Heroku

--- app/Log.php
+++ app/Log.php
@@ -10,20 +10,30 @@ class Log extends Model
 {
 
     public static $rules = [
-        'user' => 'required',
-        'base_path' => 'required',
-        'client_ip' => 'required',
-        'host' => 'required',
-        'query_string' => 'required',
-        'request_uri' => 'required',
-        'user_info' => 'required',
-        'reason' => 'required',
-        'message' => 'required',
+        'user' => 'required|string',
+        'base_path' => 'required|string',
+        'client_ip' => 'required|string',
+        'host' => 'required|string',
+        'query_string' => 'required|string',
+        'request_uri' => 'required|string',
+        'user_info' => 'required|string',
+        'reason' => 'required|string',
+        'message' => 'required|string',
     ];
 
     protected $table = 'logs';
 
     protected $primaryKey = 'id';
+
+    protected $user;
+    protected $base_path;
+    protected $client_ip;
+    protected $host;
+    protected $query_string;
+    protected $request_uri;
+    protected $user_info;
+    protected $message;
+    protected $reason;
 
     protected $fillable = [
         'user', 'base_path', 'client_ip'. 'host', 'query_string', 'request_uri', 'user_info', 'message'. 'reason',
--- app/Services/CreateLogService.php
+++ app/Services/CreateLogService.php
@@ -22,18 +22,19 @@ class CreateLogService
             return response()->json(
                 ['content' => [], 'error_messages' => $validator->errors()], Response::HTTP_BAD_REQUEST
             );
+        } else {
+            $log = new Log();
+            $log->user = $request->get('user');
+            $log->base_path = $request->get('base_path');
+            $log->client_ip = $request->get('client_ip');
+            $log->host = $request->get('host');
+            $log->query_string = $request->get('query_string');
+            $log->request_uri = $request->get('request_uri');
+            $log->user_info = $request->get('user_info');
+            $log->reason = $request->get('reason');
+            $log->message = $request->get('message');
+            $log->save();
+            return response()->json(['content' => $request->all(), 'error_messages' => []], Response::HTTP_OK);
         }
-        $log = new Log();
-        $log->user = $request->get('user');
-        $log->base_path = $request->get('base_path');
-        $log->client_ip = $request->get('client_ip');
-        $log->host = $request->get('host');
-        $log->query_string = $request->get('query_string');
-        $log->request_uri = $request->get('request_uri');
-        $log->user_info = $request->get('user_info');
-        $log->reason = $request->get('reason');
-        $log->message = $request->get('message');
-        $log->save();
-        return response()->json(['content' => $request->all(), 'error_messages' => []], Response::HTTP_OK);
     }
 }
